b623ee7bb2ed8e8b79aeded16b8729f243fbabfb
preshit1505 <preshit.dwivedi012@gmail.com>
2022-06-15 11:27:42 -0700

    Update cicd-pipeline.tf

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -1,5 +1,5 @@
 resource "aws_codebuild_project" "tf-plan" {
-  name          = "tf-cicd-plan2"
+  name          = "tf-cicd-plan"
   description   = "Plan stage for terraform"
   service_role  = aws_iam_role.tf-codebuild-role.arn
 
@@ -87,7 +87,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             owner = "AWS"
             input_artifacts = ["tf-code"]
             configuration = {
-                ProjectName = "tf-cicd-plan2"
+                ProjectName = "tf-cicd-plan"
             }
         }
     }
