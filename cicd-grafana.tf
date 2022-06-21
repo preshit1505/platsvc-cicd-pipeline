fd8c841d303456173ba5bdfe497e321c70aeb2b8
preshit1505 <preshit.dwivedi012@gmail.com>
2022-06-21 15:34:27 -0700

    Update cicd-grafana.tf

--- cicd-grafana.tf
+++ cicd-grafana.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_service_linked_role" "es" {
-    aws_service_name = "es.amazonaws.com"
+    aws_service_name = "es.amazonaws.com-test"
     description      = "Allows Amazon ES to manage AWS resources for a domain on your behalf."
     lifecycle {
     ignore_changes = all
